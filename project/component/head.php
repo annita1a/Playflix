d48e699a698dbb42efdb189137f94b449ca3e594
Peri <annitasfitri7@gmail.com>
2025-12-19 22:37:01 +0700

    blm

---
+++ project/component/head.php
@@ -0,0 +1,54 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>StreamIT | Responsive Bootstrap 5 Template</title>
+    <!-- Google Font Api KEY-->
+    <meta name="google_font_api" content="AIzaSyBG58yNdAjc20_8jAvLNSVi9E4Xhwjau_k">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./assets/images/favicon.ico" />
+
+    <!-- Library / Plugin Css Build -->
+    <link rel="stylesheet" href="./assets/css/core/libs.min.css" />
+
+    <!-- font-awesome css -->
+    <link rel="stylesheet" href="./assets/vendor/font-awesome/css/all.min.css" />
+
+    <!-- Iconly css -->
+    <link rel="stylesheet" href="./assets/vendor/iconly/css/style.css" />
+
+    <!-- Animate css -->
+    <link rel="stylesheet" href="./assets/vendor/animate.min.css" />
+
+    <!-- SwiperSlider css -->
+    <link rel="stylesheet" href="./assets/vendor/swiperSlider/swiper.min.css">
+
+
+    <!-- Sweetlaert2 css -->
+    <link rel="stylesheet" href="./assets/vendor/sweetalert2/sweetalert2.min.css" />
+
+
+
+    <!-- Streamit Design System Css -->
+    <link rel="stylesheet" href="./assets/css/streamit.min.css?v=5.4.0" />
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="./assets/css/custom.min.css?v=5.4.0" />
+
+    <!-- Rtl Css -->
+    <link rel="stylesheet" href="./assets/css/rtl.min.css?v=5.4.0" />
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300&display=swap"
+        rel="stylesheet">
+
+
+    <!-- Phosphor icons  -->
+    <link rel="stylesheet" href="./assets/vendor/phosphor-icons/Fonts/regular/style.css">
+    <link rel="stylesheet" href="./assets/vendor/phosphor-icons/Fonts/duotone/style.css">
+
+    <link rel="stylesheet" href="./assets/vendor/phosphor-icons/Fonts/fill/style.css">
+
+
+    <link rel="stylesheet" href="./assets/vendor/streamit-font/iconly.css">
